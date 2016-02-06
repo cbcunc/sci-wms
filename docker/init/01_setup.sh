b8e17e2ae8ef9b1a18631215c5b05355448d7d42
Kyle Wilcox <kyle@axiomalaska.com>
2016-02-05 22:43:01 -0500

    DJANGO_SETTINGS_MODULE is already set in Dockerfile

--- docker/init/01_setup.sh
+++ docker/init/01_setup.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-export DJANGO_SETTINGS_MODULE=overlord.settings.$SETTINGS
-echo $DJANGO_SETTINGS_MODULE > /etc/container_environment/DJANGO_SETTINGS_MODULE
-
 export DJANGO_SECRET_KEY=$(pwgen -s -B -1 40)
 echo $DJANGO_SECRET_KEY > /etc/container_environment/DJANGO_SECRET_KEY
 
